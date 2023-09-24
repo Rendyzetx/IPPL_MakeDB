16ba3bc5705bebd1821afd8caccc6511bcddaae0
DimasAliA <143428324+DimasAliA@users.noreply.github.com>
2023-09-24 17:39:47 +0800

    Update script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,7 +3,7 @@ const sendChatBtn = document.querySelector(".chat-input span");
 const chatbox = document.querySelector(".chatbox");
 
 let userMessage;
-const API_KEY = "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";
+const API_KEY = "";
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, ClassName) => {

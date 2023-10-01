42078de409e9c23b30f112e04e60d092a32a832e
DimasAliA <143428324+DimasAliA@users.noreply.github.com>
2023-10-01 17:00:29 +0800

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -3,33 +3,57 @@ import bodyParser from 'body-parser';
 import sqlRoutes from './routes/sqlRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import cors from 'cors';
-import { pool } from './utils/db.js';
+import { pool,getUserById } from './utils/db.js';
 import session from 'express-session';
 import passport from 'passport';
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const corsOptions = {
+    origin: 'http://127.0.0.1:8080',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+}
+
+app.use(cors(corsOptions));
+// app.use(cors({ origin: 'http://127.0.0.1:8080', credentials: true }));
+
 app.set('dbPool', pool);
 
-app.use('/', sqlRoutes);
-app.use('/auth', authRoutes);
+app.use(session({ 
+    secret: 'secretKey@ipplMakeDB', 
+    resave: false, 
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        sameSite: 'Lax'
+        // httpOnly: true
+    }
+}));
 
-app.use(session({ secret: 'xx', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use('/auth', authRoutes);
+app.use('/sql', sqlRoutes);
 
 passport.serializeUser((user, done) => {
-    done(null, user);
+    done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    done(null, id);
+    getUserById(id, (err, user) => {
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        return done(null, user);
+    });
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-});
+}).on('error', console.error.bind(console));

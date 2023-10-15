0a6ca5f2ef5d486789293a0ec7f5dc05d6479d2c
Rendyzet <107420488+Rendyzetx@users.noreply.github.com>
2023-10-15 16:48:52 +0800

    Update sqlRoutes.js

--- backend/routes/sqlRoutes.js
+++ backend/routes/sqlRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { relatedDb, get_namadb, check_db, fix_maxtoken } from '../utils/utils.js';
+import { relatedDb, get_namadb, check_db, fix_maxtoken,parseSqlToDiagramData } from '../utils/utils.js';
 import { logActivity } from '../utils/db.js';
 
 const router = express.Router();
@@ -31,6 +31,22 @@ router.get('/getSQLResult',(req, res) => {
     const sqlResult = res.app.locals.sqlResult || "Tidak Ada SQL yang Dihasilkan";
     
     res.json({ sql: sqlResult });
+});
+
+router.post('/generateERD', (req, res) => {
+    const sql = req.body.sql;
+    
+    if (!sql) {
+        return res.status(400).json({ error: 'SQL is required' });
+    }
+
+    try {
+        const diagramData = parseSqlToDiagramData(sql);
+        return res.status(200).json(diagramData);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 export default router;

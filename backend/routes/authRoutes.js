6f2ead26a268514e89f2ebf40f81f9cacab5189b
Rendyzet <107420488+Rendyzetx@users.noreply.github.com>
2023-10-15 16:48:30 +0800

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,13 +6,13 @@ import { pool, registerUser, logActivity } from '../utils/db.js';
 const router = express.Router();
 
 passport.use(new GoogleStrategy({
-    clientID: "xx",
-    clientSecret: "xx",
+    clientID: "571679498319-g6e9t3fapk5uisc3q91l5lfgn2l808fu.apps.googleusercontent.com",
+    clientSecret: "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",
     callbackURL: "http://localhost:3000/auth/google/callback"
 },
 async (token, tokenSecret, profile, done) => {
     try {
-        console.log('Profile received from Google:', profile);
+        // console.log('Profile received from Google:', profile);
         let [rows] = await pool.query('SELECT * FROM users WHERE google_id = ?', [profile.id]);
         if (rows.length === 0) {
             console.log('User Tidak Ditemukan, Tambahkan Ke db');
@@ -30,27 +30,41 @@ async (token, tokenSecret, profile, done) => {
     }
 }));
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google', (req, res, next) => {
+    console.log('tes1:', req.session)
+    console.log('tes2:', req.user)
+    if (req.session && req.session.user) {
+        res.redirect('http://127.0.0.1:8080/index2.html');
+    } else {
+        next();
+    }
+}, passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), async (req, res) => {
-    console.log('req sesion', req.session);
+    console.log('Session (Callback):', req.session);
+    console.log('User:', req.user);
     if(!req.user){
-        console.error('Tidak ada');
-        // return res.redirect('/');
+        console.error('User not found');
+        return res.redirect('/');
     }
+
     const { id, displayName, emails } = req.user;
     const userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    req.session.userId = id;
-    req.session.displayName = displayName;
-    req.session.email = emails[0].value
+    req.session.user = {
+        userId: id,
+        displayName: displayName,
+        email: emails[0].value
+    };
+
     try {
         await new Promise((resolve, reject) => {
             req.session.save(err => {
                 if(err) {
-                    console.error('gagal save session:', err);
+                    console.error('Failed to save session:', err);
                     reject(err);
                 } else {
+                    console.log('Session saved successfully');
                     resolve();
                 }
             });
@@ -63,10 +77,49 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
     res.redirect('http://127.0.0.1:8080/index2.html');
 });
 
+router.get('/check-login-status', (req, res) => {
+    console.log('Checking login status...');
+    console.log('Session:', req.session);
+    console.log('User ID:', req.session.userId);
+    if (req.isAuthenticated()) {
+        res.json({ isLoggedIn: true });
+    } else {
+        res.json({ isLoggedIn: false });
+    }
+});
+router.get('/check-session', (req, res) => {
+    console.log('Session (check-session):', req.session);
+    console.log('User(check-session):', req.user);
+    if(req.session && req.session.userId) {
+        res.send('Session exists, user ID: ' + req.session.userId);
+    } else {
+        res.send('No session found!');
+    }
+});
+
 router.get('/logout', (req, res) => {
-    req.logout(() => {
+    console.log('logouttttttt:', req.user)
+    if (req.isAuthenticated()) {
+        console.log('User is authenticated, trying to log out...');
+        try {
+            req.logout();
+            req.session.destroy((err) => {
+                if (err) {
+                    console.error('Error destroying session:', err);
+                } else {
+                    console.log('Session destroyed, clearing cookie...');
+                    res.clearCookie('connect.sid');
+                    console.log('Redirecting to home...');
+                    res.redirect('/');
+                }
+            });
+        } catch (err) {
+            console.error('Error during logout:', err);
+        }
+    } else {
+        console.log('User is not authenticated, redirecting...');
         res.redirect('http://127.0.0.1:8080/index.html');
-    });
+    }
 });
 
 

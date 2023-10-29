a9966d63b60de51763ed45c35437f029ebc1cc41
DimasAliA <143428324+DimasAliA@users.noreply.github.com>
2023-10-29 18:10:53 +0800

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,10 +1,12 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'make_db_akun'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
 };
 
 const pool = mysql.createPool(dbConfig);
